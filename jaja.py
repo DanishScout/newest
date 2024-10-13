fcb96b3bde4a391d4170c52a9e4a260288c30786
DanishScout <kristianjuul1999@gmail.com>
2024-10-13 19:10:23 +0200

    Add files via upload

--- jaja.py
+++ jaja.py
@@ -10,27 +10,50 @@ st.markdown("<h1 style='font-size: 34px;'>Make Your Own Data Visualizations</h1>
 # Add a subtitle with adjusted margin for closer positioning
 st.markdown("<h2 style='font-size: 16px; font-weight: normal; margin-top: -32px;'>Data Updated Weekly | By <a href='https://x.com/DanishScout_' target='_blank'>DanishScout</a> | <a href='https://buymeacoffee.com/danishscout' target='_blank'>Support My Work</a></h2>", unsafe_allow_html=True)
 
+# Add a checkbox to show available leagues
+if st.sidebar.checkbox("Show Available Leagues"):
+    leagues = """
+    - Superligaen
+    - Allsvenskan
+    - Eliteserien
+    - Premier League
+    - LaLiga
+    - Bundesliga
+    - Serie A
+    - Ligue 1
+    - Eredivisie
+    - Liga Portugal
+    - First Division A
+    - Premiership
+    - J. League
+    - Liga Profesional
+    - Brazilian Serie A
+    - Austrian Bundesliga
+    """
+    st.sidebar.markdown(leagues)
+
 # Add a horizontal line
 st.markdown("<hr style='border: 1px solid black;'>", unsafe_allow_html=True)
 
-# Add a title for the app in the main area
-st.sidebar.title("Select Visualization")
-
-# Add a dropdown for user selection of visualization in the main area
-visualization_option = st.sidebar.selectbox(
+# Use radio buttons for visualization selection
+visualization_option = st.radio(
     "Choose which visualization to display:",
-    ("pizza", "radar", "scatter", "rapport")
+    ("Pizza Chart", "Radar Comparison", "Scatter Plot", "Match Report")
 )
 
+# Add a horizontal line
+st.markdown("<hr style='border: 1px solid black;'>", unsafe_allow_html=True)
+
 # Run the selected visualization
-if visualization_option == "pizza":
+if visualization_option == "Pizza Chart":
     pizza.run()  # Assuming the pizza script has a run() function
 
-elif visualization_option == "radar":
+elif visualization_option == "Radar Comparison":
     radar.run()
 
-elif visualization_option == "scatter":
+elif visualization_option == "Scatter Plot":
     scatter.run()
 
-elif visualization_option == "rapport":
+elif visualization_option == "Match Report":
     rapport.run()
+
